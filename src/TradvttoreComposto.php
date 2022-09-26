2552ee061956b479b2827e3fc2503ae46c0baadd
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2022-09-26 19:16:25 +0200

    Introdvzione del tradvttore composto

--- src/TradvttoreComposto.php
+++ src/TradvttoreComposto.php
@@ -6,7 +6,20 @@ namespace LVI;
 
 class TradvttoreComposto implements PassoDiTradvzione
 {
+    /**
+     * @var PassoDiTradvzione[]
+     */
+    private array $passi;
+
+    public function __construct(PassoDiTradvzione ...$passi)
+    {
+        $this->passi = $passi;
+    }
+
     public function tradvci(string $messaggio): string
     {
+        return array_reduce($this->passi, function (string $messaggio, PassoDiTradvzione $passoDiTradvzione) {
+            return $passoDiTradvzione->tradvci($messaggio);
+        }, $messaggio);
     }
 }
