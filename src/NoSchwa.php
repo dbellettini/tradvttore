a601745188ff66a9dca51172d0b1881e1f3e553e
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2022-09-26 23:12:46 +0200

    Fissa errori di disciplina del codice

--- src/NoSchwa.php
+++ src/NoSchwa.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LVI;
 
 class NoSchwa implements PassoDiTradvzione
--- tests/NoSchwaTest.php
+++ tests/NoSchwaTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LVI\Tests;
 
 use LVI\NoSchwa;
