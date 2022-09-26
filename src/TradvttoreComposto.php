aa85ee07340f2cbcbf82eeebb2721a284932e814
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2022-09-26 19:13:22 +0200

    Aggiungi dvmper

---
+++ src/TradvttoreComposto.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LVI;
+
+class TradvttoreComposto implements PassoDiTradvzione
+{
+    public function tradvci(string $messaggio): string
+    {
+    }
+}
