194eba01a38135b544d576b0a85e23b5dee05917
Davide Bellettini <325358+dbellettini@users.noreply.github.com>
2022-09-26 21:17:42 +0200

    Configvrazione del magro

--- pubblica/index.php
+++ pubblica/index.php
@@ -1,1 +1,15 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $name = 'World';
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+$app->run();
